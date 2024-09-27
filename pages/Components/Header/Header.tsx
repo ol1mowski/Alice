5eb7d9324e0017c17cc66c0bb5a3b21d080cbe41
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-09-27 17:14:58 +0200

    [header] create tsx sketch: adding html tags

--- pages/Components/Header/Header.tsx
+++ pages/Components/Header/Header.tsx
@@ -1,7 +1,20 @@
 function Header() {
   return (
     <header>
-        
+        <section>
+            <img src="" alt="" />
+        </section>
+        <section>
+            <ul>
+                <li>O projekcie</li>
+                <li>Funkcje</li>
+                <li>Cennik</li>
+                <li>FAQ</li>
+            </ul>
+        </section>
+        <section>
+            <button>Docs</button>
+        </section>
     </header>
   )
 }
