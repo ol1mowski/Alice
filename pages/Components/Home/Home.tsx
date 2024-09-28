64d09987eb2953238ec54490bc31b173e1f3eb71
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-09-28 13:46:33 +0200

    [Home] Home component: add TSX tags

--- pages/Components/Home/Home.tsx
+++ pages/Components/Home/Home.tsx
@@ -1,9 +1,39 @@
+import Image from "next/image";
+
 function Home() {
   return (
-    <main>
-        
+    <main className="flex flex-col">
+      <section className="hidden">
+        <Image
+          src={
+            "https://res.cloudinary.com/dbbuav0rj/image/upload/v1727523443/AliceImage/aliceMainImage_jzkp7e.webp"
+          }
+          alt="alice main image"
+          width={500}
+          height={500}
+        />
+      </section>
+      <section>
+        <div>
+          <span>+300</span>
+          <span>Atywnych Użytkowników</span>
+          <span>+700</span>
+          <span>Zapytań Dziennie</span>
+          <span>+400</span>
+          <span>Pozytywnych Opini</span>
+        </div>
+        <div>
+          <div>
+            <span>Sprawdź teraz</span>
+            <Image src={"https://res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/arrow_qdzrqv.svg"} alt="arrow icon" width={50} height={50} />
+          </div>
+          <h1>
+            Poznaj nasze najnowsze rozwiązanie <span>AI</span>
+          </h1>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
