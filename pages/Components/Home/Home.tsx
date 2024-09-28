aadc1058b0c598d92cd9cef39d7be97e69a60410
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-09-28 14:17:59 +0200

    [Home] home design: add style to mobile home tags

--- pages/Components/Home/Home.tsx
+++ pages/Components/Home/Home.tsx
@@ -13,53 +13,53 @@ function Home() {
           height={500}
         />
       </section>
-      <section className="h-2/3">
-        <div className="h-1/3 pt-20 flex justify-between">
+      <section className="h-full">
+        <div className="h-1/4 mt-40 flex justify-between">
           <div className="flex flex-col">
-            <span className="font-bold text-4xl">+300</span>
+            <span className="font-bold text-purple text-4xl">+300</span>
             <span className="text-l">
               Atywnych <br /> Użytkowników
             </span>
           </div>
           <div className="flex flex-col">
-            <span className="font-bold text-4xl">+900</span>
+            <span className="font-bold text-purple text-4xl">+900</span>
             <span className="text-l">
               Zapytań <br /> Dziennie
             </span>
           </div>
           <div className="flex flex-col">
-            <span className="font-bold text-4xl">+40</span>
+            <span className="font-bold text-purple text-4xl">+40</span>
             <span className="text-l">
               Pozytywnych <br /> Opinii
             </span>
           </div>
         </div>
-        <div>
-          <div>
-            <span>Sprawdź teraz</span>
+        <div className="flex flex-col gap-10">
+          <div className="flex items-end gap-5">
+            <span className="text-3xl text-purple">Sprawdź teraz</span>
             <Image
               src={
                 "https://res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/arrow_qdzrqv.svg"
               }
               alt="arrow icon"
-              width={50}
-              height={50}
+              width={40}
+              height={40}
             />
           </div>
-          <h1>
-            Poznaj nasze najnowsze rozwiązanie <span>AI</span>
+          <h1 className="text-5xl leading-tight font-semibold">
+            Poznaj nasze najnowsze rozwiązanie <span className="text-purple">AI</span>
           </h1>
         </div>
       </section>
 
-      <section>
+      <section className="flex justify-center items-center animate-bounce mt-10 h-auto">
         <Image
           src={
             "https://res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/scroll_jjgqji.svg"
           }
           alt="scroll icon"
-          width={50}
-          height={50}
+          width={75}
+          height={75}
         />
       </section>
     </main>
