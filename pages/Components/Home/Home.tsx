0844359f01fe94b074b7ee85314de597b26883c8
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-09-28 13:59:40 +0200

    [Home] home info charts style: add style to info charts

--- pages/Components/Home/Home.tsx
+++ pages/Components/Home/Home.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col h-screen p-5">
       <section className="hidden">
         <Image
           src={
@@ -13,14 +13,26 @@ function Home() {
           height={500}
         />
       </section>
-      <section>
-        <div>
-          <span>+300</span>
-          <span>Atywnych Użytkowników</span>
-          <span>+700</span>
-          <span>Zapytań Dziennie</span>
-          <span>+400</span>
-          <span>Pozytywnych Opini</span>
+      <section className="h-2/3">
+        <div className="h-1/3 pt-20 flex justify-between">
+          <div className="flex flex-col">
+            <span className="font-bold text-4xl">+300</span>
+            <span className="text-l">
+              Atywnych <br /> Użytkowników
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold text-4xl">+900</span>
+            <span className="text-l">
+              Zapytań <br /> Dziennie
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold text-4xl">+40</span>
+            <span className="text-l">
+              Pozytywnych <br /> Opinii
+            </span>
+          </div>
         </div>
         <div>
           <div>
@@ -41,14 +53,14 @@ function Home() {
       </section>
 
       <section>
-          <Image
-              src={
-                "https://res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/scroll_jjgqji.svg"
-              }
-              alt="scroll icon"
-              width={50}
-              height={50}
-            />
+        <Image
+          src={
+            "https://res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/scroll_jjgqji.svg"
+          }
+          alt="scroll icon"
+          width={50}
+          height={50}
+        />
       </section>
     </main>
   );
