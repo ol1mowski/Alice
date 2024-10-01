e46bda5b3f64a2f99f1d70ef8276d63ea2c05a55
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-01 19:36:18 +0200

    [Header] header RWD: improve header Responsive

--- pages/Components/Header/MobileMenu/HeaderMobileMenu.tsx
+++ pages/Components/Header/MobileMenu/HeaderMobileMenu.tsx
@@ -17,27 +17,27 @@ function HeaderMobileMenu({
   return (
     <nav
       ref={mobileMenuOpen}
-      className="hidden w-full fixed animate-menu inset-0 bg-black bg-opacity-70 z-50 flex-col gap-20 items-center justify-center"
+      className="hidden w-full fixed animate-menu inset-0 bg-black bg-opacity-80 z-50 flex-col gap-10 items-center justify-center"
     >
       <section
         ref={mobileMenuClose}
         onClick={closeMobileMenuHandler}
         className="flex items-center justify-start absolute top-20 right-20"
       >
-        <button className="h-20 w-20 text-4xl border rounded border-white text-white hover:scale-105 transition duration-700">
+        <button className="h-14 w-14 text-4xl border rounded border-white text-white hover:scale-105 transition duration-700">
           x
         </button>
       </section>
       <section className="flex">
         <ul className="flex justify-center items-start flex-col gap-5 text-2xl text-white space-y-4">
-          <li className="text-5xl">O projekcie</li>
-          <li className="text-5xl">Funkcje</li>
-          <li className="text-5xl">Cennik</li>
-          <li className="text-5xl">FAQ</li>
+          <li className="text-4xl">O projekcie</li>
+          <li className="text-4xl">Funkcje</li>
+          <li className="text-4xl">Cennik</li>
+          <li className="text-4xl">FAQ</li>
         </ul>
       </section>
       <section className="flex items-center justify-start">
-        <button className="h-20 w-60 text-3xl border rounded border-white text-white hover:scale-105 transition duration-700">
+        <button className="h-16 w-52 text-3xl border rounded border-white text-white hover:scale-105 transition duration-700">
           Docs
         </button>
       </section>
