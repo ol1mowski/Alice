674628b475370d931ea1585a15774196db48cc9d
Oliwier <oliwier.markiewicz.dev@gmail.com>
2024-10-03 18:01:24 +0200

    [AboutProject] animation: remove animation code

--- pages/Components/AboutProject/AboutProject.tsx
+++ pages/Components/AboutProject/AboutProject.tsx
@@ -1,17 +1,14 @@
 import AboutIcon from "./AboutIcon/AboutIcon.component";
 import AboutContent from "./AboutContent/AboutContent.component";
 import AboutWrapper from "./AboutWrapper/AboutWrapper.component";
-import { RefObject } from "react";
 
-function AboutProject({about}: {about: RefObject<HTMLDivElement>}) {
+function AboutProject() {
   return (
-    <section ref={about} className="h-screen absolute top-100 bg-white">
     <AboutWrapper>
       <AboutIcon className="absolute top-0 left-0 z-0" />
       <AboutContent />
       <AboutIcon className="absolute bottom-0 right-0 z-0" />
     </AboutWrapper>
-    </section>
   );
 }
 
--- pages/Components/Home/Home.tsx
+++ pages/Components/Home/Home.tsx
@@ -4,42 +4,42 @@ import HomeImage from "./HomeImage/HomeImage";
 import HomeCtaWrapper from "./HomeCtaWrapper/HomeCtaWrapper";
 import HomeInfoCharts from "./HomeInfoCharts/HomeInfoCharts";
 import HomeInfoCta from "./HomeInfoCta/HomeInfoCta";
-import HomeScrollComponent from './HomeScrollComponent/<section className="flex justify-center items-center animate-bounce mt-14 h-auto sm:mt-20 lg:hidden">         <Image           src={             "https:/res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/scroll_jjgqji.svg"           }           alt="scroll icon"           width={75}           height={75}         />       </HomeScrollComponent';
-import AboutProject from "../AboutProject/AboutProject";
 
-import { useEffect, useRef, useState } from "react";
+// import AboutProject from "../AboutProject/AboutProject";
+
+import HomeScrollComponent from "./HomeScrollComponent/<section className=\"flex justify-center items-center animate-bounce mt-14 h-auto sm:mt-20 lg:hidden\">         <Image           src={             \"https:/res.cloudinary.com/dbbuav0rj/image/upload/v1727450427/AliceIcons/scroll_jjgqji.svg\"           }           alt=\"scroll icon\"           width={75}           height={75}         />       </HomeScrollComponent";
 
 function Home() {
-  const about = useRef<HTMLDivElement>(null);
+  // const about = useRef<HTMLDivElement>(null);
 
-  const [height, setHeight] = useState<number>(100);
+  // const [height, setHeight] = useState<number>(100);
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    const handleScroll = (e: WheelEvent) => {
-      setHeight((prevHeight) => {
-        if (e.deltaY > 0) {
-          if (prevHeight > 0) {
-            return Math.max(prevHeight - 5, 0);
-          }
-        } else if (e.deltaY < 0) {
-          return Math.min(prevHeight + 5, 100);
-        }
-        return prevHeight;
-      });
+  // useEffect(() => {
+  //   document.body.style.overflow = "hidden";
+  //   const handleScroll = (e: WheelEvent) => {
+  //     setHeight((prevHeight) => {
+  //       if (e.deltaY > 0) {
+  //         if (prevHeight > 0) {
+  //           return Math.max(prevHeight - 5, 0);
+  //         }
+  //       } else if (e.deltaY < 0) {
+  //         return Math.min(prevHeight + 5, 100);
+  //       }
+  //       return prevHeight;
+  //     });
 
-      if (about.current) {
-        about.current.style.top = `${height}vh`;
-      }
-    };
+  //     if (about.current) {
+  //       about.current.style.top = `${height}vh`;
+  //     }
+  //   };
 
-    window.addEventListener("wheel", handleScroll);
+  //   window.addEventListener("wheel", handleScroll);
 
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
-      document.body.style.overflow = "auto";
-    };
-  }, [height]);
+  //   return () => {
+  //     window.removeEventListener("wheel", handleScroll);
+  //     document.body.style.overflow = "auto";
+  //   };
+  // }, [height]);
 
   return (
     <>
@@ -52,7 +52,7 @@ function Home() {
         </Home.ctaWrapper>
         <Home.scroll />
       </HomeWrapper>
-      <AboutProject about={about} />
+      {/* <AboutProject about={about} /> */}
     </>
   );
 }
